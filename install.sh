237feda6e860e8dc7eaf4e3e5816618e3d8c92ef
hbseo <Hanbae Seo>
2020-11-30 11:25:31 +0900

    add new plugins

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 if [ -e ~/.vimrc -o -e ~/.vim ]; then
-	echo "경고: 설치를 진행하려면 ~/.vim/ 디렉토리와 ~/.vimrc 파일을 삭제해야 합니다."
+	echo "warning: delete ~/.vim/ and ~/.vimrc"
 	exit
 fi
 
-echo "심볼릭 릭크 생성..."
+echo "create symbolic link..."
 ln -sfv ~/vimconfig/vimrc ~/.vimrc
 
-echo "vundle 다운로드중..."
+echo "vundle downloading..."
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-echo "vundle 설치중..."
-vi -c :BundleInstall -c :qa
+echo "install plugins by hbseo"
+mkdir ~/.vim/autoload/
+cp autoload/* ~/.vim/autoload/
+
+echo "vundle installing..."
+vim -c :BundleInstall -c :qa
